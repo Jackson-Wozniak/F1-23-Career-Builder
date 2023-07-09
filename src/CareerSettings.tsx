7c455bdf49efc46a93db557fb090c0bd19d3fac4
Jackson-Wozniak <jjw30@comcast.net>
2023-07-09 19:22:06 -0400

    change images

--- src/CareerSettings.tsx
+++ src/CareerSettings.tsx
@@ -3,9 +3,12 @@ import './CareerSettings.css';
 const CareerSettings = (props: any) => {
     return ( 
         <div className="career-settings">
+            <h3>Start your career with...</h3>
+
             <img src={require('./images/' + props.team.imgLink)} alt="logo" className="team-logo"/>
 
             <h4>Teammate: {props.teammate}</h4>
+            
         </div>
     );
 }
